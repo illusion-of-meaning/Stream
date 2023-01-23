187f6f9380fa53560f028e5edfef62af1872f076
AScherbakov <illusion-of-meaning@ya.ru>
2023-01-23 19:38:33 +0900

    for check

--- src/main/java/com/asherbakov/Main.java
+++ src/main/java/com/asherbakov/Main.java
@@ -1,7 +1,18 @@
 package main.java.com.asherbakov;
 
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        new TaskSecond().printEvenNumbers(1,8,12,6,48,24,23,11);
+        // task 1
+        Stream<Integer> stream = Stream.of(8, 16, 32, 1, 9, -12, 0);
+
+        new TaskFirst().findMinMax(stream,
+                Integer::compareTo,
+                (a1, a2) -> System.out.printf("Min: %s; max: %s%n", a1, a2)
+        );
+
+        // task 2
+        new TaskSecond().printEvenNumbers(1, 8, 12, 6, 48, 24, 23, 11);
     }
 }
--- src/main/java/com/asherbakov/TaskFirst.java
+++ src/main/java/com/asherbakov/TaskFirst.java
@@ -1,7 +1,9 @@
 package main.java.com.asherbakov;
 
 import java.util.Comparator;
+import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class TaskFirst {
@@ -9,6 +11,11 @@ public class TaskFirst {
             Stream<? extends T> stream,
             Comparator<? super T> order,
             BiConsumer<? super T, ? super T> minMaxConsumer) {
-
+        List<T> list = stream.sorted(order).collect(Collectors.toList());
+        if (!list.isEmpty()) {
+            minMaxConsumer.accept(list.get(0), list.get(list.size() - 1));
+        } else {
+            minMaxConsumer.accept(null, null);
+        }
     }
 }
